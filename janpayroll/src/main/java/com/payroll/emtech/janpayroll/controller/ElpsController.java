76e562e1c7e467ec63fd2ad7263c5fc5586700d7
MutemiMwalimu <mutemimwalimu70@gmail.com>
2024-02-16 12:26:56 +0300

    first commit

--- janpayroll/src/main/java/com/payroll/emtech/janpayroll/controller/ElpsController.java
+++ janpayroll/src/main/java/com/payroll/emtech/janpayroll/controller/ElpsController.java
@@ -1,25 +1,54 @@
 package com.payroll.emtech.janpayroll.controller;
 
+import com.payroll.emtech.janpayroll.exception.ResourceNotFoundException;
 import com.payroll.emtech.janpayroll.model.Elps;
 import com.payroll.emtech.janpayroll.repository.ElpsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/Elps")
-
+@RequestMapping("/api/v1/elps")
 public class ElpsController {
 
     @Autowired
-    public ElpsRepository ELPSRepository;
+    private ElpsRepository elpsRepository;
 
     @GetMapping
-    public List<Elps> getAllElps(){
-        return ELPSRepository.findAll();
+    public List<Elps> getAllElps() {
+        return elpsRepository.findAll();
+    }
+
+    @PostMapping
+    public Elps createElps(@RequestBody Elps elps) {
+        return elpsRepository.save(elps);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Elps> getElpsById(@PathVariable long id) {
+        Elps elps = elpsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Elps not exist with id: " + id));
+        return ResponseEntity.ok(elps);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Elps> updateElps(@PathVariable long id, @RequestBody Elps updatedElps) {
+        Elps elps = elpsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Elps not exist with id: " + id));
+
+
+        Elps updatedElpsEntity = elpsRepository.save(elps);
+        return ResponseEntity.ok(updatedElpsEntity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteElpsById(@PathVariable long id) {
+        Elps elps = elpsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Elps not exist with id: " + id));
+
+        elpsRepository.delete(elps);
+        return ResponseEntity.noContent().build();
     }
 }
-
